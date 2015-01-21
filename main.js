739f17f584f3b758a645747219f99037fe15041e
Imee Cuison <imeetwelve@gmail.com>
2015-01-21 13:54:56 -0500

    navbar

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function)(); {
+
+
+//our nav links
+  var $album1 = $('nav a').eq(0);
+  var $album2 = $('nav a').eq(1);
+  var $album3 = $('nav a').eq(2);
+  var $album4 = $('nav a').eq(3);
+  var $album5 = $('nav a').eq(4);
+  var $album6 = $('nav a').eq(5);
+
+  
+
+
+
+
+
+
+
+
+}
