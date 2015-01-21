342b62b17b2ba7f84c93c70768230c89a7c37392
Imee Cuison <imeetwelve@gmail.com>
2015-01-21 18:17:27 -0500

    morealbums

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ $(document).ready(function() {
 
 //our nav links
   var $Album1 = $('.Album1');
-  var $img1 = $('.img1')
-//  var $album2 = $('.').eq(1);
-//  var $album3 = $('nav a').eq(2);
-//  var $album4 = $('nav a').eq(3);
-//  var $album5 = $('nav a').eq(4);
-//  var $album6 = $('nav a').eq(5);
+  var $img1 = $('.img1');
+  var $img21 =$('.img21');
+  var $img31=$('.img31');
+  var $backbutton = $(".backbutton");
+  var $Album2 = $('.Album2');
+  var $img12 = $('.img12');
+  var $img22 =$('.img22');
+  var $img32=$('.img32');
+
 
 $Album1.click(function (event){
   event.preventDefault();
@@ -16,24 +19,114 @@ $Album1.click(function (event){
 //  $(this).closest("img").children().addClass("active");
 //  $('.Album1').children().addClass("active");
   $(this).closest("div").siblings().addClass("hide");
-  $(".img2").addClass("active");
+  $(".img21").addClass("active");
   //$(this).parent().addClass("hide");
-  $(".img3").addClass("active");
+  $(".img31").addClass("active");
+
+  //$(this).closest("img").children().("active");
+ //$(this).children("img").addClass("active");
  $(".menu").addClass("active");
 
 //  $('.Album1').siblings().addClass("hide");
 
+$img1.click(function(event){
+  event.preventDefault();
+
+  $(this).addClass("big");
+  //$(this).closest("img").siblings().addClass("hide");
+  $(this).closest("a").siblings("a").addClass("hide");
+  //$(".img1").siblings().removeClass("active");
+  $(".backbutton").addClass("active");
+
+  //$(".img2").removeClass("active");
+  //$(".img3").removeClass("active");
+//  $(".img2").addClass("hide");
+  //$(".img3").addClass("hide");
 
 });
 
-//$img1.click(function(event){
-  //event.preventDefault();
-  //$(this).closest("img").siblings().addClass("hide");
+$img21.click(function(event){
+  event.preventDefault();
+
+  $(this).addClass("big");
+  $(this).closest("a").siblings("a").addClass("hide");
+  $(".backbutton").addClass("active");
+});
+
+$img31.click(function(event){
+  event.preventDefault();
+
+  $(this).addClass("big");
+  $(this).closest("a").siblings("a").addClass("hide");
+  $(".backbutton").addClass("active");
+
+});
+$backbutton.click(function(event){
+  event.preventDefault();
+  //$(this).closest("a").siblings("a").addClass("active");
+  //$(".img21").addClass("active");
+  //$(this).children("a").addClass("hide");
+  $(".img1").removeClass("big");
+  $(".img21").addClass("hide");
+  $(".img31").addClass("hide");
+  $(".Album1").removeClass("hide");
+
+});
 
 
 
-//});
+});
 
+$Album2.click(function (event){
+  event.preventDefault();
+  //console.log(this);
+  //  $(this).closest("img").children().addClass("active");
+  //  $('.Album1').children().addClass("active");
+  $(this).closest("div").siblings().addClass("hide");
+  $(".img22").addClass("active");
+  //$(this).parent().addClass("hide");
+  $(".img32").addClass("active");
+
+  //$(this).closest("img").children().("active");
+  //$(this).children("img").addClass("active");
+  $(".menu").addClass("active");
+
+  //  $('.Album1').siblings().addClass("hide");
+
+  $img12.click(function(event){
+    event.preventDefault();
+
+    $(this).addClass("big");
+    //$(this).closest("img").siblings().addClass("hide");
+    $(this).closest("a").siblings("a").addClass("hide");
+    //$(".img1").siblings().removeClass("active");
+    $(".backbutton").addClass("active");
+
+    //$(".img2").removeClass("active");
+    //$(".img3").removeClass("active");
+    //  $(".img2").addClass("hide");
+    //$(".img3").addClass("hide");
+
+  });
+
+  $img22.click(function(event){
+    event.preventDefault();
+
+    $(this).addClass("big");
+    $(this).closest("a").siblings("a").addClass("hide");
+    $(".backbutton").addClass("active");
+  });
+
+  $img32.click(function(event){
+    event.preventDefault();
+
+    $(this).addClass("big");
+    $(this).closest("a").siblings("a").addClass("hide");
+    $(".backbutton").addClass("active");
+
+  });
+
+});
 
 
 
