9748c04f547dbd43db12eaa528262ff9ef73bd40
Imee Cuison <imeetwelve@gmail.com>
2015-01-21 16:23:01 -0500

    album1appears

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 //our nav links
   var $Album1 = $('.Album1');
-
+  var $img1 = $('.img1')
 //  var $album2 = $('.').eq(1);
 //  var $album3 = $('nav a').eq(2);
 //  var $album4 = $('nav a').eq(3);
@@ -12,19 +12,27 @@ $(document).ready(function() {
 
 $Album1.click(function (event){
   event.preventDefault();
-console.log(this);
+//console.log(this);
 //  $(this).closest("img").children().addClass("active");
+//  $('.Album1').children().addClass("active");
   $(this).closest("div").siblings().addClass("hide");
-  $(this).getElementbyClass("menu").addClass("active");
+  $(".img2").addClass("active");
+  //$(this).parent().addClass("hide");
+  $(".img3").addClass("active");
+ $(".menu").addClass("active");
 
-
-  $(this).getElementbyClass("img2").addClass("active");
-
-  $('.Album1').siblings().addClass("hide");
+//  $('.Album1').siblings().addClass("hide");
 
 
 });
 
+//$img1.click(function(event){
+  //event.preventDefault();
+  //$(this).closest("img").siblings().addClass("hide");
+
+
+
+//});
 
 
 
